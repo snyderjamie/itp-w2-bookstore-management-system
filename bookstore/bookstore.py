fbb506ccba1619fa97e2583ec295ac5b3522f7ab
snyderj <jamielsnyder@gmail.com>
2019-07-19 02:35:52 +0000

    added changes to bookstore.py in order to pass all tests, but there is an error still

--- bookstore/bookstore.py
+++ bookstore/bookstore.py
@@ -1,26 +1,65 @@
+# we are builging a collection, not extracting a collection from string
 def create_bookstore(name):
-    pass
+    # setter and getter function
+    # store = name
+    store = {
+        'name': name,
+        'authors': [],
+        'books': []
+    }
+    return store
 
 
 def get_bookstore_name(bookstore):
-    pass
+    # this is the getter portion of the fuction
+    return bookstore['name']
 
 
 def add_author(bookstore, name, nationality):
-    pass
+    # from the assert statement we found out that we need 
+    # to the creat a dictionary
+    # store is the first value, 
+    author = {
+        'name': name, 
+        'nationality': nationality,
+    }
+    bookstore['authors'].append(author)
+
+    return author
 
 
 def get_author_by_name(bookstore, name):
-    pass
-
+    # for author in bookstore['authors']:
+    #    if name == author['name']:
+    #       return author
+    author_list = bookstore['authors']
+    
+    for author in author_list:
+        if name == author['name']:
+            return author
 
 def add_book(bookstore, title, isbn, author):
-    pass
+    book = {
+        'title' = title, 
+        'isbn' = isbn,
+        'author' = author
+    }
+    bookstore['book'].append(book)
+    return book
 
 
 def get_book_by_title(bookstore, title):
-    pass
+    book_list = bookstore['book']
+    for title in book['title']:
+        if title == book['title']:
+            return book
 
 
 def get_books_by_author(bookstore, author):
-    pass
+    books = []
+    book_list = bookstore['books']
+    for book in book_list:
+        if author == book['author']:
+            books.append(book)
+    return books
+    
